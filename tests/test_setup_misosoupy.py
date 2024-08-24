f19b13776cb464b6e6ff0e1b675cc9f32ec89c2e
danibene <34680344+danibene@users.noreply.github.com>
2024-08-24 01:36:39 -0400

    fix format

--- tests/test_setup_misosoupy.py
+++ tests/test_setup_misosoupy.py
@@ -1,19 +1,20 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 from misosoupy import setup_misosoupy
+
 
 def test_get_home_dir():
     """
     Tests the functionality of the get_home_dir function.
-    
-    Verifies that the returned home directory path exists and is a directory, 
+
+    Verifies that the returned home directory path exists and is a directory,
     that its name matches the expected 'misosoupy' directory, and that the
     current working directory is the returned home directory.
     """
     home_dir = setup_misosoupy.get_home_dir()
     assert Path(home_dir).is_dir()
-    assert Path(home_dir).name == 'misosoupy'
+    assert Path(home_dir).name == "misosoupy"
     # Not really sure why it is necessary to change the cwd
     # But it is what the function is stated to do
     assert os.getcwd() == home_dir
